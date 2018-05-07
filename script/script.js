206ba701e39460c68c86e768b303b158c2e0f29a
makskornakov <makskornakov9180@gmail.com>
2018-05-07 16:19:25 +0300

    ver:animate

---
+++ script/script.js
@@ -0,0 +1,37 @@
+var $torf_s = false;
+var $torf_m = false;
+function searchclick(){
+	if ($torf_s == false) {
+		document.getElementById('search_id').style.width = '380px';
+		document.getElementById('search_id').style.background = '#f55500';
+		document.getElementById('input_id').style.display = 'block';
+		document.getElementById('input_id').focus();
+		$torf_s = true;
+		if ($torf_m == true) {
+			menuclick()
+		}
+	}
+	else if ($torf_s == true){
+		document.getElementById('search_id').style.width = '73px';
+		document.getElementById('search_id').style.background = '';
+		document.getElementById('input_id').style.display = 'none';
+		$torf_s = false;
+	}
+}
+function menuclick(){
+	if ($torf_m == false) {
+		document.getElementById('open_menu_id').style.width = '700px';
+		document.getElementById('menu_animate').style.transform = 'rotate(90deg)';
+		document.getElementById('menu_animate2').style.transform = 'rotate(90deg)';
+		$torf_m = true;
+		if ($torf_s == true) {
+			searchclick()
+		}
+	}
+	else if ($torf_m == true) {
+		document.getElementById('open_menu_id').style.width = '0px';
+		document.getElementById('menu_animate').style.transform = 'none';
+		document.getElementById('menu_animate2').style.transform = 'none';
+		$torf_m = false;
+	}
+}
